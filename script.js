1e9d9c650aed673631494d6aa140d4a91262c187
Nokuthula Khumalo <noksmbulase@gmail.com>
2025-01-09 21:39:22 +0200

    Added the basic search engine functionalities

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Hello girl!");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerElement = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
